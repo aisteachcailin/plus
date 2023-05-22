b335f171dbb3fae208602d768406db83ef51fbc7
Anastasia <73635691+ganyushinaanastasia@users.noreply.github.com>
2023-05-22 16:16:44 +0300

    popup

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 import '../scss/style.scss';
+import '../scss/popup.scss';
+import '../../node_modules/@fancyapps/ui/dist/fancybox/fancybox.css';
+import { Fancybox } from '@fancyapps/ui';
+
+Fancybox.bind("[data-fancybox]", {
+    dragToClose: false
+  });
